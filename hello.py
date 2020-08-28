e2a0c928d27db8da31901985201684c3cf5b66e3
K3nny13 <kenneth_cogzell@hotmail.com>
2020-08-28 11:34:06 +0200

    added templates folder

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Test</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def greet(name):
-    return f'Hello {name}'
+    return render_template('user.html', name=name)
